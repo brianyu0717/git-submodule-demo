62a96de59cbe5338f7c820745430a6d3b332688c
brian <brian@joinblink.com>
2016-09-10 13:24:03 +0100

    added submodule project2

--- src/main/java/com/example/projects/Project1.java
+++ src/main/java/com/example/projects/Project1.java
@@ -3,6 +3,7 @@ package com.example.projects;
 public class Project1 {
     public static void main(String[] args) {
         System.out.println("Hello from project1");
+        System.out.println(Project2.printFromProject2());
     }
 
     public static String printFromProject1(){
